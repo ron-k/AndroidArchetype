22f01d6095b750abedc42a62409c139228d46c8b
Ron K <ron.kassay@gmail.com>
2022-07-14 20:56:04 +0300

    removed dagger

--- app/src/main/java/com/ronk/archetype/android/Application.java
+++ app/src/main/java/com/ronk/archetype/android/Application.java
@@ -2,24 +2,23 @@ package com.ronk.archetype.android;
 
 import androidx.annotation.NonNull;
 
+import java.util.Objects;
+
 /**
  * Created by ronk on 10/04/2017.
  */
 
 public class Application extends android.app.Application {
-
-    private ApplicationComponent applicationComponent;
+    private static Application instance;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        applicationComponent = DaggerApplicationComponent.builder()
-                .applicationModule(new ApplicationModule(this))
-                .build();
+        instance = this;
     }
 
     @NonNull
-    public ApplicationComponent getApplicationComponent() {
-        return applicationComponent;
+    public static Application getInstance() {
+        return Objects.requireNonNull(instance);
     }
 }
